b4d0ecf1ebd112f039774d33fad0327cd955b185
PRITAM RAUTH <prauth2023@gmail.com>
2024-04-13 09:39:03 +0530

    done

---
+++ list.py
@@ -0,0 +1,19 @@
+#list
+marks = ["Prith",3,55,66,33.0] #strings are immutable but We can acces the value
+                               #list are muteable (Changeable )and we can acess also
+
+#if we try to add motre element which is out of the index in list it gives us error 
+#we can also do slicing of list
+#slicing format 
+#lst_name[starting_index:ending_index]
+
+print(marks)
+marks[0]="y"
+print(marks)
+print(len(marks))
+
+print("*"*70)
+
+str="hello"
+print("str[0]")
+#str[0]="y" we can't
---
+++ list_method.py
@@ -0,0 +1,7 @@
+list=[1,2,3]
+list.append(9) #add element at the end
+list.sort()#sort in ascending order
+list.sort(reverse=True)#sort in decending order
+list.insert(9,2)
+list.reverse()
+print(list)
